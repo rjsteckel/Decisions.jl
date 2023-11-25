6f2e5a0ff2f68e382447fe08b22b6050e1598d5b
Documenter.jl <documenter@juliadocs.github.io>
2023-11-25 15:36:51 +0000

    build based on 5d619fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Decisions","category":"page"},{"location":"#Decisions","page":"Home","title":"Decisions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Decisions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Decisions]","category":"page"}]
+}
