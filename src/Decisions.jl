3d5e763fecd214d46f02077648179c103e01a535
Ryan Steckel <rsteckel@gmail.com>
2024-02-03 11:24:40 -0600

    updates

--- src/Decisions.jl
+++ src/Decisions.jl
@@ -4,12 +4,12 @@ module Decisions
 using NamedArrays
 using IterTools
 
-include("losses.jl")
 include("observations.jl")
+include("losses.jl")
+
 
 export ObservationMatrix
 export LossMatrix
-
 export expected_strategy_loss
 export bayes_strategy_loss
 
